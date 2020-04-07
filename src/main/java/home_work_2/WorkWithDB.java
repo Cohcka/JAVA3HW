a9d7253ad6a123a2a5aeb4246767bcf8bd6f2b7d
Cohcka <Cohcka@gmail.com>
2020-04-07 22:52:30 +0300

    Ready HW

--- src/main/java/home_work_2/WorkWithDB.java
+++ src/main/java/home_work_2/WorkWithDB.java
@@ -1,13 +1,15 @@
 package home_work_2;
 
-import java.sql.DriverManager;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
 
 public class WorkWithDB {
 
     private static Connection connection;
     private static Statement statement;
-    private final String file = "DZ_update.txt";
+    private static final String file = "DZ_update.txt";
 
     public static void main (String[] args){
         final String tableName = "Students";
@@ -21,9 +23,14 @@ public class WorkWithDB {
         createNewTable(tableName);
         insert(tableName, "Bob", 12);
         insert(tableName, "Marley", 75);
+        insert(tableName, "Viki", 15);
+        insert(tableName, "Mark", 55);
+        insert(tableName, "Max", 5);
+        insert(tableName, "Sid", 75);
+
         select(tableName, "Marley");
         update(tableName, "Marley", 100);
-        delete(tableName, "Bob");
+        delete(tableName, "Sid");
         updateDataFromTXT(tableName, file);
         disconnect();
     }
@@ -59,9 +66,12 @@ public class WorkWithDB {
     }
 
     private static void select(String tableName, String studName){
-        String sql = "SELECT score from " + tableName + " WHERE name = '" + studName + "';";
+        String sql = "SELECT id, name, score from " + tableName + " WHERE name = '" + studName + "';";
         try {
             ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()){
+                System.out.println(rs.getInt(1)+ " " + rs.getString(2)+ " " + rs.getString(3));
+            }
             // вывести на экран результаты поиска
         } catch (SQLException e) {
             e.printStackTrace();
@@ -70,6 +80,15 @@ public class WorkWithDB {
 
     private static void update (String tableName, String studName, int setScore){
         String sql = "UPDATE " + tableName + " SET score = " + setScore + " WHERE name = '" + studName + "';";
+        try {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void update (String tableName, String id, String setScore){
+        String sql = "UPDATE " + tableName + " SET score = " + setScore + " WHERE id = " + id + ";";
         try {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
@@ -87,6 +106,29 @@ public class WorkWithDB {
     }
 
     private static void updateDataFromTXT(String tableName, String fileName){
+        StringBuffer bodyFile = new StringBuffer();
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            int i;
+            while ((i=fis.read()) != -1){
+                bodyFile.append((char)i);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] data = bodyFile.toString().split("\n");
+        String[][] data2 = new String[data.length][];
+        for (int i = 0; i < data.length; i++) {
+            data2[i] = data[i].split("  ", 3);
+        }
+
+        for (int i = 0; i < data2.length; i++) {
+            update(tableName, data2[i][0], data2[i][2]);
+        }
     }
 
     private static void disconnect(){
